be7e194a55a8ff2526faeec5396f4dde7c813c7b
Deadinside <kostyadevyatov@gmail.com>
2021-04-07 19:13:57 +0400

    buttons work with error

--- app/src/main/java/com/example/joteroapp/MainActivity.kt
+++ app/src/main/java/com/example/joteroapp/MainActivity.kt
@@ -3,17 +3,27 @@ package com.example.joteroapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.View.VISIBLE
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_quiz_feed.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         button_to_quiz_feed.setOnClickListener {
             val quizFeedIntent = Intent(this, QuizFeed::class.java)
             startActivity(quizFeedIntent)
         }
-
+        button_yes.setOnClickListener {
+            val quizFeedIntent = Intent(this, QuizFeed::class.java)
+            no_available_activity?.setVisibility(View.INVISIBLE)
+            startActivity(quizFeedIntent)
+        }
+        button_no.setOnClickListener {
+            val quizFeedIntent = Intent(this, QuizFeed::class.java)
+            startActivity(quizFeedIntent)
+        }
     }
 }
--- app/src/main/java/com/example/joteroapp/QuizFeed.kt
+++ app/src/main/java/com/example/joteroapp/QuizFeed.kt
@@ -3,13 +3,13 @@ package com.example.joteroapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import kotlinx.android.synthetic.main.activity_quiz_feed.*
 
 class QuizFeed : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quiz_feed)
-
         available_quiz_body.setOnClickListener {
             val quizExampleIntent = Intent(this, MainActivity::class.java)
             startActivity(quizExampleIntent)
